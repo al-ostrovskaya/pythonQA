35718cbf0bd79ca0786935c65484a0682906d311
schiwa13 <42767613+schiwa13@users.noreply.github.com>
2024-02-20 00:30:07 +0300

    Initial commit

---
+++ python lesson6_step8.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+link = "http://suninjuly.github.io/find_xpath_form"
+
+
+def version1():
+
+    input1 = browser.find_element(By.TAG_NAME, "input")
+    input1.send_keys("Ivan")
+    input2 = browser.find_element(By.NAME, "last_name")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element(By.CLASS_NAME, "city")
+    input3.send_keys("Smolensk")
+    input4 = browser.find_element(By.ID, "country")
+    input4.send_keys("Russia")
+    button = browser.find_element(By.XPATH, "//button[@type='submit']").click()
+    time.sleep(5)
+
+def version2():
+    input1 = browser.find_element(By.TAG_NAME, "input")
+    input1.send_keys("Ivan")
+    input2 = browser.find_element(By.NAME, "last_name")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element(By.CLASS_NAME, "city")
+    input3.send_keys("Smolensk")
+    input4 = browser.find_element(By.ID, "country")
+    input4.send_keys("Russia")
+    button = browser.find_element(By.XPATH, "/html/body/div/form/div[6]/button[3]").click()
+    time.sleep(5)
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+    version2()
+
+finally:
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
