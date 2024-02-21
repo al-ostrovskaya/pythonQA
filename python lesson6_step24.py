fddd405bc5ea65ca3e7e0461b4d6f6acd75a5253
schiwa13 <42767613+schiwa13@users.noreply.github.com>
2024-02-21 21:24:52 +0300

    Ожидание появления элементов перед кликом

---
+++ python lesson6_step24.py
@@ -0,0 +1,31 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import math
+import time
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+try:
+    browser = webdriver.Chrome()
+
+    browser.get("http://suninjuly.github.io/explicit_wait2.html")
+    button = WebDriverWait(browser, 12).until(
+            EC.text_to_be_present_in_element((By.ID,'price'), "$100")
+        )
+    button = browser.find_element(By.CSS_SELECTOR, "#book").click()
+    x_element = browser.find_element(By.CSS_SELECTOR, "#input_value")
+    x = x_element.text
+    y = calc(x)
+    answer= browser.find_element(By.CSS_SELECTOR, "#answer")
+    answer.send_keys(y)
+    button = browser.find_element(By.CSS_SELECTOR, "#solve").click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+
